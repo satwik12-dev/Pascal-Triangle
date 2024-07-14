3ea5e525303335d9e11cd94e4517448e9d7ed261
satwik12-dev <satwiksaxena41@gmail.com>
2024-07-14 13:02:17 +0530

    Pascal Triangle.cpp

--- Pascal Triangle.cpp
+++ Pascal Triangle.cpp
@@ -1,8 +1,8 @@
 /*           1 
            1 2 1
           1 3 3 1
-		 1 4 6 4 1
-	   1 5 10 10 5 1
+	 1 4 6 4 1
+	1 5 10 10 5 1
 		
 This is known as pascal triangle
 Approach it can be done by cpmbinatioon of rows and column 
